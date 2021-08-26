e61579cc719c63db091ef2ce6cf132a7e47c7564
zhangyiwen <zzyyyww9595@gmail.com>
2021-08-26 16:41:39 +0800

    Separate the compilation of each db

--- db/db_factory.cc
+++ db/db_factory.cc
@@ -15,8 +15,13 @@
 #include "db/tbb_rand_db.h"
 #include "db/tbb_scan_db.h"
 //#include "db/log_db.h.back"
+#ifdef USING_PMEM_ROCKSDB
 #include "db/pmem_rocksdb_db.h"
+#endif
+
+#ifdef USING_CCEH
 #include "db/cceh_db.h"
+#endif
 
 using namespace std;
 using ycsbc::DB;
@@ -37,10 +42,14 @@ DB* DBFactory::CreateDB(utils::Properties &props) {
     return new TbbScanDB;
   //} else if (props["dbname"] == "logdb"){
   //    return new LogDB;
+#ifdef USING_PMEM_ROCKSDB
   } else if (props["dbname"] == "pmem-rocksdb"){
       return new ycsb_pmem_rocksdb::PmemRocksDB;
+#endif
+#ifdef USING_CCEH
   } else if (props["dbname"] == "cceh"){
       return new cceh_db::CCEHDB;
+#endif
   }
   else return NULL;
 }
